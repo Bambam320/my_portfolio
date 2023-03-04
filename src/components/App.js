b902640897c450a1e59c9a6ae98fb8dc1a193f13
phase5-capstone <igor.mestechkin@gmail.com>
2023-03-03 23:36:52 -0500

    update app component with routes

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/about" element={<About/>} />
+      <Route path="/skills" element={<Skills/>} />
+      <Route path="/projects" element={<Project/>} />
+      <Route path="/algorithms" element={<Algorithms/>} />
+      <Route path="/blogs" element={<Blogs/>} />
+      <Route path="/contact" element={<Contact/>} />
+    </Routes>
   );
 }
 
