7db7999583c63560019ffccfcc7452a9c8278a8d
phase5-capstone <igor.mestechkin@gmail.com>
2023-03-22 13:16:18 -0400

    update navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom';
+
 
 import "../CSS/Navbar.css";
 
 function Navbar() {
 
   return (
-    <div>Igor Mestechkin</div>
+    <>
+      <ul className="nav-menu">
+        <li className="nav-item">
+          <NavLink 
+            to="/" 
+            className="nav-link" 
+            activeClassName="active"
+          >
+            <h1 id="first-Nav-link">
+              Home
+            </h1>
+          </NavLink>
+        </li> 
+      </ul>
+    </>
   )
 }
 
-export default Navbar
+export default Navbar;
