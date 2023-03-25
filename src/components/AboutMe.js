fd479a1b10593c96f060b3d764f733be37b71c6a
phase5-capstone <igor.mestechkin@gmail.com>
2023-03-25 05:52:37 -0400

    update about

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,67 @@ import '../CSS/AboutMe.css';
 
 function AboutMe() {
   return (
-    <div>AboutMe</div>
+    <div className="about-container">
+      <div className="about-left-box">
+        <h1 className="about-left-title">
+          <span id="about-W">W</span>
+          <span id="about-E">E</span>
+          <span id="about-L">L</span>
+          <span id="about-C">C</span>
+          <span id="about-O">O</span>
+          <span id="about-M">M</span>
+          <span id="about-Ex2">E</span>
+          <span id="about-exclamation">!</span>&nbsp;
+          <span id="about-I">I</span>
+          <span id="about-apostrophe">'</span>
+          <span id="about-Mx2">M</span>&nbsp;
+          <span id="about-Ix2">I</span>
+          <span id="about-G">G</span>
+          <span id="about-Ox2">O</span>
+          <span id="about-R">R</span>
+          <span id="about-period">.</span>
+          <br />
+          <span id="about-F">F</span>
+          <span id="about-U">U</span>
+          <span id="about-Lx2">L</span>
+          <span id="about-Lx3">L</span>&nbsp;
+          <span id="about-S">S</span>
+          <span id="about-T">T</span>
+          <span id="about-A">A</span>
+          <span id="about-Cx2">C</span>
+          <span id="about-K">K</span>&nbsp;
+          <span id="about-D">D</span>
+          <span id="about-Ex3">E</span>
+          <span id="about-V">V</span>
+          <span id="about-Ex4">E</span>
+          <span id="about-Lx4">L</span>
+          <span id="about-Ox3">O</span>
+          <span id="about-P">P</span>
+          <span id="about-Ex5">E</span>
+          <span id="about-Rx2">R</span>
+          <br />
+          <span id="about-Ax2">A</span>
+          <span id="about-N">N</span>
+          <span id="about-Dx2">D</span>&nbsp;
+          <span id="about-Lx5">L</span>
+          <span id="about-Ix2">I</span>
+          <span id="about-Fx2">F</span>
+          <span id="about-Ex6">E</span>
+          <span id="about-Lx6">L</span>
+          <span id="about-Ox4">O</span>
+          <span id="about-Nx2">N</span>
+          <span id="about-Gx2">G</span>&nbsp;
+          <span id="about-Lx7">L</span>
+          <span id="about-Ex7">E</span>
+          <span id="about-Ax3">A</span>
+          <span id="about-Rx3">R</span>
+          <span id="about-Nx3">N</span>
+          <span id="about-Ex8">E</span>
+          <span id="about-Rx4">R</span>
+          <span id="about-period2">.</span>
+        </h1>
+      </div>
+    </div>
   )
 }
 
