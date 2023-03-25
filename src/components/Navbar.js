af341463498447e17bea129f7f37ec4d7e812182
phase5-capstone <igor.mestechkin@gmail.com>
2023-03-25 02:30:54 -0400

    update navbar info about me

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { NavLink } from 'react-router-dom';
 
 
@@ -6,99 +6,114 @@ import "../CSS/Navbar.css";
 
 function Navbar() {
 
+
+  useEffect(() => {
+    let style = document.body.style
+    navActive ? 
+      style.overflow = 'hidden'
+    :
+      style.overflow = 'unset'
+  }, [navActive])
+
   return (
     <>
       <div className={navActive ? "navbar-container navActive" : "navbar-container"}>
-      <div className="navbar-top">
-        <img src={emblem} alt="IM" onClick={() => handleLogoClick} />
-        <h2> Igor Mestechkin </h2>
-        <p> Full Stack Developer </p>
-      </div>
-      <ul className="nav-menu">
-        <li className="nav-item">
-          <NavLink 
-            to="/" 
-            className="nav-link" 
-            activeClassName="active"
-          >
-            <h1>
-              Home
-            </h1>
-          </NavLink>
-        </li> 
-        <li className="nav-item">
-          <NavLink
-            to="/aboutme"
-            className="nav-link"
-            activeClassName="active"
-          >
-            <h1>
-              About Me
-            </h1>
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="skill"
-            className="nav-link"
-            activeClassName="active"
-          >
-            <hi>
-              Skills
-            </hi>
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/experience"
-            className="nav-link"
-            activeClassName="active"
-          >
-            <h1>
-              Experience
-            </h1>
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/technical"
-            className="nav-link"
-            activeClassName="active"
-          >
-            <h1>
-              Technical
-            </h1>
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/blog"
-            className="nav-link"
-            activeClassName="active"
-          >
-            <h1>
-              Blog
-            </h1>
-          </NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink
-            to="/contact"
-            className="nav-link"
-            activeClassName="active"
+        <div className="navbar-top">
+          <img src={emblem} alt="IM" onClick={() => handleLogoClick} />
+          <h2> Igor Mestechkin </h2>
+          <p> Full Stack Developer </p>
+        </div>
+        <ul className="nav-menu">
+          <li className="nav-item">
+            <NavLink
+              to="/"
+              className="nav-link"
+              activeClassName="active"
             >
-            <h1>
-              Contact
-            </h1>
-          </NavLink>
-        </li>
-      </ul>
-      <div className="navbar-bottom">
-        <a href="" target="_blank" rel="noopener noreferrer">
-          
-        </a>
+              <h1>
+                Home
+              </h1>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/aboutme"
+              className="nav-link"
+              activeClassName="active"
+            >
+              <h1>
+                About Me
+              </h1>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="skill"
+              className="nav-link"
+              activeClassName="active"
+            >
+              <hi>
+                Skills
+              </hi>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/experience"
+              className="nav-link"
+              activeClassName="active"
+            >
+              <h1>
+                Experience
+              </h1>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/technical"
+              className="nav-link"
+              activeClassName="active"
+            >
+              <h1>
+                Technical
+              </h1>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/blog"
+              className="nav-link"
+              activeClassName="active"
+            >
+              <h1>
+                Blog
+              </h1>
+            </NavLink>
+          </li>
+          <li className="nav-item">
+            <NavLink
+              to="/contact"
+              className="nav-link"
+              activeClassName="active"
+            >
+              <h1>
+                Contact
+              </h1>
+            </NavLink>
+          </li>
+        </ul>
+        <div className="navbar-bottom">
+          <a href="https://www.linkedin.com/in/igor-mestechkin-519a97109/" target="_blank" rel="noopener noreferrer" >
+            <FaLinkedin className='nav-icon' />
+          </a>
+          <a href="https://github.com/Bambam320" target="_blank" rel="noopener noreferrer" >
+            <FaGithub className='nav-icon' />
+          </a>
+          <a href="https://medium.com/@igor.mestechkin" target="_blank" rel="noopener noreferrer">
+            <FaMedium className='nav-icon' />
+          </a>
+        </div>
       </div>
-            </div>
     </>
   )
 }
