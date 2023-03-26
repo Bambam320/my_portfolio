4cd0b89e770dce7337f75fff5895c429ee671cc7
phase5-capstone <igor.mestechkin@gmail.com>
2023-03-26 00:37:21 -0400

    update aboutme

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,11 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import '../CSS/Skill.css';
 
 function Skill() {
   return (
-    <div>Skill</div>
+    <div className='skills-container'>
+      <div className='skills-wrapper'>
+        <h1 className='skills-header'>
+          <span>K</span>
+          <span>n</span>
+          <span>o</span>
+          <span>w</span>
+          <span>l</span>
+          <span>e</span>
+          <span>d</span>
+          <span>g</span>
+          <span>e</span>&nbsp
+          <span>&</span>&nbsp
+          <span>C</span>
+          <span>a</span>
+          <span>p</span>
+          <span>a</span>
+          <span>b</span>
+          <span>i</span>
+          <span>l</span>
+          <span>i</span>
+          <span>t</span>
+          <span>i</span>
+          <span>e</span>
+          <span>s</span>
+        </h1>
+      </div>
+    </div>
   )
 }
 
-export default Skill
+export default Skill;
