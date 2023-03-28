de9340f123c707cbb24608af1d1cbcf00c1ff587
phase5-capstone <igor.mestechkin@gmail.com>
2023-03-27 23:16:59 -0400

    update skills

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -30,6 +30,24 @@ function Skill() {
           <span>e</span>
           <span>s</span>
         </h1>
+        <p className='skills-description'>
+          <TypeWriterEffect 
+            startDelay={1000} 
+            cursorColor="white" 
+            typeSpeed={35}
+            hideCursorAfterText={true}
+            text="I'm a lifelong learner whose chosen the perfect career!" 
+          /> &nbsp;&nbsp;
+          <span id="skills-mobile-second">
+            <TypeWriterEffect 
+              startDelay={4000} 
+              cursorColor="white"
+              typeSpeed={35}
+              hideCursorAfterText={true}
+              text="Proficient and constantly improving in the following technologies."
+            />
+          </span>
+        </p>
       </div>
     </div>
   )
