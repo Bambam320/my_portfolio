f8e59a027e13f710f10328204734831b82091fa7
phase5-capstone <igor.mestechkin@gmail.com>
2023-03-28 00:35:38 -0400

    update skills

--- src/components/Skill.js
+++ src/components/Skill.js
@@ -48,8 +48,76 @@ function Skill() {
             />
           </span>
         </p>
+        <div className='skills-box'>
+          <h1 className='skills-title'>Frontend</h1>
+          <div className='skills-content'>
+            <div id="ReactSkill" className='skills-item'>
+              <img src={ReactImg} alt="React" />
+              <h2>React JS</h2>
+              <p>My preferred framework</p>
+            </div>
+            <div id="ReactSkill" className="skill-separator"></div>
+            <div id="ReduxSkill" className="skills-item">
+              <img src={ReduxImg} alt="Redux" />
+              <h2>Redux</h2>
+              <p>Redux Description</p>
+            </div>
+            <div id="ReduxSkill" className="skillSeparator"></div>
+            <div id="JavascriptSkill" className="skills-item">
+              <img src={JavascriptImg} alt="Javascript" />
+              <h2>Javascript</h2>
+              <p>JS Description</p>
+            </div>
+            <div id="JavascriptSkill" className="skillSeparator hiddenSkillSeparator"></div>
+            <div id="CSSSkill" className="skills-item responsiveSkillsItem1">
+              <img src={CSSImg} alt="CSS" />
+              <h2>CSS</h2>
+              <p>CSS description</p>
+            </div>
+            <div id="CSSSkill" className="skillSeparator responsiveSkillsItem2"></div>
+            <div id="HTMLSkill" className="skills-item responsiveSkillsItem3">
+              <img src={HTMLImg} alt="HTML" />
+              <h2>HTML</h2>
+              <p>HTML description</p>
+            </div>
+          </div>
+        </div>
+        <div className="skills-box">
+          <h1 className="skills-title">Backend</h1>
+          <div className="skills-content">
+              <div id="RubySkill" className="skills-item">
+                <img src={RubyImg} alt="Ruby" />
+                <h2>Ruby</h2>
+                <p>Ruby description</p>
+              </div>
+              <div id="RubySkill" className="skillSeparator"></div>
+              <div id="RailsSkill" className="skills-item">
+                <img src={RailsImg} alt="Rails" />
+                <h2>Rails</h2>
+                <p>Rails description</p>
+              </div>
+              <div id="RailsSkill" className="skillSeparator"></div>
+              <div id="PostgreSQLSkill" className="skills-item">
+                <img src={PostgreSQLImg} alt="PostgreSQL" />
+                <h2>PostgreSQL</h2>
+                <p>PostgreSQL description</p>
+              </div>
+              <div id="PostgreSQLSkill" className="skillSeparator hiddenSkillSeparator"></div>
+              <div id="SQLSkill" className="skills-item responsiveSkillsItem1">
+                <img src={SQLImg} alt="SQL" />
+                <h2>SQL</h2>
+                <p>SQL description</p>
+              </div>
+              <div id="VSCodeSkill" className="skillSeparator responsiveSkillsItem2"></div>
+              <div id="VSCodeSkill" className="skills-item responsiveSkillsItem3">
+                <img src={VSCodeImg} alt="VSCode" />
+                <h2>VSCode</h2>
+                <p>VSCode description</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
   )
 }
 
