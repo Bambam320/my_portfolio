79eaee625077a9c523da6483eb50af6c07bdbb0b
phase5-capstone <igor.mestechkin@gmail.com>
2023-04-03 17:49:55 -0400

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,7 +4,24 @@ import '../CSS/Blog.css';
 
 function Contact() {
   return (
-    <div>Contact</div>
+    <div className='contact-container'>
+      <div className="contact-left-box">
+        <h1 className="contact-title">
+          <span id='contact-C'>C</span>
+          <span id='contact-O'>o</span>
+          <span id='contact-N'>n</span>
+          <span id='contact-T'>t</span>
+          <span id='contact-A'>a</span>
+          <span id='contact-Cx2'>c</span>
+          <span id='contact-Tx2'>t</span>&nbsp;
+          <span id='contact-M'>M</span>
+          <span id='contact-E'>e</span>
+        </h1>
+        <p className="contact-message contactForm1">
+          I would be happy to connect and collaborate on open source projects, technical problem solving practice or just to chat!
+        </p>
+      </div>
+    </div>
   )
 }
 
