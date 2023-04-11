8450ce2714123e0f45ec2a6ff1bf11af9dca06bd
phase5-capstone <igor.mestechkin@gmail.com>
2023-04-10 23:44:29 -0400

    update experience js file

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,10 +2,34 @@ import React from 'react'
 
 import '../CSS/Experience.css';
 
+
 function Experience() {
+
+  const otherData = [
+    {id: 1, title: 'Calculator', image: WeatherLogo, github: "github address", link: "link to github io page", largeTitle: false},
+  ]
+
   return (
-    <div>Experience</div>
+    <div className='work-container'>
+      <Modal open={openModal} setOpenModal={setOpenModal} modalID={modalID}/>
+      <div className="port-wrapper">
+        <h1 className="port-header">
+          <span>M</span>
+          <span>y</span>&nbsp;
+          <span>P</span>
+          <span>o</span>
+          <span>r</span>
+          <span>t</span>
+          <span>f</span>
+          <span>o</span>
+          <span>l</span>
+          <span>i</span>
+          <span>o</span>
+        </h1>
+      </div>
+    </div>
   )
+
 }
 
-export default Experience
+export default Experience;
