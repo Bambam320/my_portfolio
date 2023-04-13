3a582dbb058441a9cde3a4f209b338688bd8f48f
phase5-capstone <igor.mestechkin@gmail.com>
2023-04-12 22:57:10 -0400

    update experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -63,13 +63,38 @@ function Experience() {
               <a className="port-title-site" href='render website for fakeify' target="_blank" rel="noreferrer">www.Fakeify.com</a>
               <p className="port-headline">A spotify clone website that allows users to save songs to their linked Spotify account!</p>
               <div className="port-btn-box">
-                  <div className='port-btn-more' onClick={() => handleOpenModal(1)}>
-                      <span id='port-more-text'>More Details</span>
-                      <BsArrowRight id='port-more-arrow'/>
-                  </div>
+                <div className='port-btn-more' onClick={() => handleOpenModal(1)}>
+                  <span id='port-more-text'>More Details</span>
+                  <BsArrowRight id='port-more-arrow'/>
+                </div>
               </div>
+            </div>
           </div>
-          </div> 
+          <div className="port-item" id='portItem2' onMouseEnter={() => setPort2(true)} onMouseLeave={() => setPort2(false)}>
+            <div className="port-gif-box">
+              <div className="port-gif-wrapper">
+                <img className='port-gif' src={Your-Vet} alt="Your-Vet Gif" style={port2 ? {opacity: "1"} : {opacity: "0"}}/>
+                <img className='port-gif port-gif-logo' src={Your-VetLogo} alt="Your-VetLogo" style={port2 ? {opacity: "0"} : {opacity: "1"}}/>
+              </div>
+              <a href='Your-vet hosted link' target="_blank" rel="noreferrer">
+                <div className="portGifPopup">
+                  <p>Go to site</p>
+                  <BsBoxArrowUpRight id='goToSitePort'/>
+                </div>
+              </a>
+            </div>
+            <div className="port-text-box">
+              <h1 className="port-title">Your-Vet<span>&trade;</span></h1>
+              <a className="port-title-site" href='your-vet hosted link' target="_blank" rel="noreferrer">www.Your-Vet.com</a>
+              <p className="port-headline">Sign in as a pet or a vet to create and check appointments.</p>
+              <div className="port-btn-box">
+                <div className='port-btn-more' onClick={() => handleOpenModal(2)}>
+                  <span id='port-more-text'>More Details</span>
+                  <BsArrowRight id='port-more-arrow'/>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
