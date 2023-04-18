44d9ab4035cfdf0f32a5c09f4f1328842dbad731
phase5-capstone <igor.mestechkin@gmail.com>
2023-04-17 22:38:54 -0400

    updated technical

--- src/components/Technical.js
+++ src/components/Technical.js
@@ -4,7 +4,26 @@ import '../CSS/Technical.css';
 
 function Technical() {
   return (
-    <div>Technical</div>
+    <>
+      <div>Technical</div>
+      <div id='algoJavascriptRuby' className="algo-item algo-item3">
+        <div className="algo-title-box">
+            <p>Javascript <span>&</span> <span>Ruby</span></p>
+            <p>2 Min</p>
+        </div>
+        <div className="algo-iframe-wrapper">
+          <iframe 
+            width="560" 
+            height="315" 
+            src="https://www.youtube.com/embed/uy5zOUKYgyo"
+            title="Palindrome"
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture"
+            allowFullScreen="allowfullscreen"
+            ></iframe>
+        </div>
+      </div>
+    </>
   )
 }
 
