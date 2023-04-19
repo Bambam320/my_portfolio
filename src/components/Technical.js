d6893f1db84794ad48ff68ad0b46b43d0ce658dc
phase5-capstone <igor.mestechkin@gmail.com>
2023-04-19 19:15:02 -0400

    updated technical

--- src/components/Technical.js
+++ src/components/Technical.js
@@ -6,19 +6,36 @@ function Technical() {
   return (
     <>
       <div>Technical</div>
-      <div id='algoJavascriptRuby' className="algo-item algo-item3">
-        <div className="algo-title-box">
+      <div id='techJavascriptRuby' className="tech-item tech-item3">
+        <div className="tech-title-box">
             <p>Javascript <span>&</span> <span>Ruby</span></p>
-            <p>2 Min</p>
+            <p>add time length</p>
         </div>
-        <div className="algo-iframe-wrapper">
+        <div className="tech-iframe-wrapper">
           <iframe 
             width="560" 
             height="315" 
-            src="https://www.youtube.com/embed/uy5zOUKYgyo"
-            title="Palindrome"
+            src="youtube for calc problem"
+            title="calculator"
             frameborder="0" 
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture" // research these allowances
+            allowFullScreen="allowfullscreen"
+            ></iframe>
+        </div>
+      </div>
+      <div id='techJavascript' className="tech-item tech-item3">
+        <div className="tech-title-box">
+            <p>Javascript </p>
+            <p>add timelength</p>
+        </div>
+        <div className="tech-iframe-wrapper">
+          <iframe 
+            width="560" 
+            height="315" 
+            src="youtube for recursion problem problem"
+            title="calculator"
+            frameborder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; fullscreen; gyroscope; picture-in-picture" // research these allowances
             allowFullScreen="allowfullscreen"
             ></iframe>
         </div>
@@ -27,4 +44,4 @@ function Technical() {
   )
 }
 
-export default Technical
+export default Technical;
