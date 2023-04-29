e250cf43bdc2a3698a681fff8e122fef91591cae
phase5-capstone <igor.mestechkin@gmail.com>
2023-04-28 23:57:01 -0400

    update blogs

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -60,6 +60,25 @@ function Blog() {
               </div>
             </div>
           </label>
+          <label style={blog2 ? {cursor: 'inherit'} : {cursor: "pointer"}} className="blog-card" htmlFor="blog-item-2" id="blog-slide-2" onClick={() => handleBlog2()}>
+            <div className="blog-content">
+              <img className='blog-image' src={image here} alt="Image Here" />
+              <div className="blog-text-content">
+                <h1 className='blog-title'><a style={blog2 ? null : {pointerEvents: "none"}} href='url of blog' target="_blank" rel="noreferrer">Name of blog</a></h1>
+                <div className="blog-start-wrap">
+                  <p className='blog-start'> Start blog summary<span id='blogStartResponsive'> End blog summary</span>...&nbsp; <a style={blog2 ? null : {pointerEvents: "none"}} href='URL of blog' target="_blank" rel="noreferrer">Read More</a></p>
+                </div>
+              </div>
+              <div className="blog-facts">
+                <h4 className="blog-date">Date</h4>
+                <h2 className="blog-read">Length of reading time</h2>
+                <div className="blog-likes">
+                  <img src={MediumClap} alt="Claps" />
+                  <p>Add like count here</p>
+                </div>
+              </div>
+            </div>
+          </label>
         </div>
       </div>
     </div>
