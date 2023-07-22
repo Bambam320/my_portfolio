dd70869e365cc26e60ef2268ccc5c7e49c141810
capstone <igor.mestechkin@gmail.com>
2023-07-22 06:46:27 -0400

    update about

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -33,54 +33,82 @@ function AboutMe() {
           <span id="about-O">O</span>
           <span id="about-M">M</span>
           <span id="about-Ex2">E</span>
-          <span id="about-exclamation">!</span>&nbsp;
+          <span id="about-wave">👋</span>
+          <br />
+          <span id="about-Mx2">M</span>
+          <span id="about-Y">Y</span>&nbsp;
+
+          <span id="about-N">N</span>
+          <span id="about-A">A</span>
+          <span id="about-Mx3">M</span>
+          <span id="about-Ex3">E</span>&nbsp;
+
           <span id="about-I">I</span>
-          <span id="about-apostrophe">'</span>
-          <span id="about-Mx2">M</span>&nbsp;
+          <span id="about-S">S</span>&nbsp;
+
           <span id="about-Ix2">I</span>
           <span id="about-G">G</span>
           <span id="about-Ox2">O</span>
-          <span id="about-R">R</span>
-          <span id="about-period">.</span>
+          <span id="about-R">R</span>&nbsp;
+
+          <span id="about-Mx4">M</span>
+          <span id="about-Ex4">E</span>
+          <span id="about-Sx2">S</span>
+          <span id="about-T">T</span>
+          <span id="about-Ex5">E</span>
+          <span id="about-Cx2">C</span>
+          <span id="about-H">H</span>
+          <span id="about-K">K</span>
+          <span id="about-Ix3">I</span>
+          <span id="about-Nx2">N</span>
           <br />
+          <span id="about-Ix4">I</span>
+          <span id="about-apostrophe">'</span>
+          <span id="about-Mx5">M</span>&nbsp;
+
+          <span id="about-Ax2">A</span>&nbsp;
+
           <span id="about-F">F</span>
           <span id="about-U">U</span>
           <span id="about-Lx2">L</span>
           <span id="about-Lx3">L</span>&nbsp;
-          <span id="about-S">S</span>
-          <span id="about-T">T</span>
-          <span id="about-A">A</span>
-          <span id="about-Cx2">C</span>
-          <span id="about-K">K</span>&nbsp;
+
+          <span id="about-Sx3">S</span>
+          <span id="about-Tx2">T</span>
+          <span id="about-Ax3">A</span>
+          <span id="about-Cx3">C</span>
+          <span id="about-Kx2">K</span>&nbsp;
+
           <span id="about-D">D</span>
-          <span id="about-Ex3">E</span>
+          <span id="about-Ex6">E</span>
           <span id="about-V">V</span>
-          <span id="about-Ex4">E</span>
+          <span id="about-Ex7">E</span>
           <span id="about-Lx4">L</span>
           <span id="about-Ox3">O</span>
           <span id="about-P">P</span>
-          <span id="about-Ex5">E</span>
-          <span id="about-Rx2">R</span>
-          <br />
-          <span id="about-Ax2">A</span>
-          <span id="about-N">N</span>
+          <span id="about-Ex8">E</span>
+          <span id="about-Rx2">R</span>&nbsp;
+
+          <span id="about-Ax4">A</span>
+          <span id="about-Nx3">N</span>
           <span id="about-Dx2">D</span>&nbsp;
+
           <span id="about-Lx5">L</span>
-          <span id="about-Ix2">I</span>
+          <span id="about-Ix5">I</span>
           <span id="about-Fx2">F</span>
-          <span id="about-Ex6">E</span>
+          <span id="about-Ex9">E</span>
           <span id="about-Lx6">L</span>
           <span id="about-Ox4">O</span>
-          <span id="about-Nx2">N</span>
+          <span id="about-Nx4">N</span>
           <span id="about-Gx2">G</span>&nbsp;
+
           <span id="about-Lx7">L</span>
-          <span id="about-Ex7">E</span>
-          <span id="about-Ax3">A</span>
+          <span id="about-Ex10">E</span>
+          <span id="about-Ax5">A</span>
           <span id="about-Rx3">R</span>
-          <span id="about-Nx3">N</span>
-          <span id="about-Ex8">E</span>
+          <span id="about-Nx5">N</span>
+          <span id="about-Ex11">E</span>
           <span id="about-Rx4">R</span>
-          <span id="about-period2">.</span>
         </h1>
         {/* Add true true and console log description with typing animation */}
         <p className='about-left-description'>
